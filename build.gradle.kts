c3d17c2eef39c161e2b7918269a96628aebbebc4
amungwat marion jones <amungwat.marion@icloud.com>
2026-03-23 19:49:16 +0100

    kotlin exercise

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,56 +1,14 @@
-import org.jetbrains.compose.desktop.application.dsl.TargetFormat
-
 plugins {
-    kotlin("jvm") version "1.9.22"
-    id("org.jetbrains.compose") version "1.5.12"
+    kotlin("jvm") version "1.9.0"
 }
 
-group = "com.gradecalculator"
-version = "1.0.0"
+group = "com.example"
+version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
-    google()
-    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
 }
 
 dependencies {
-    // Compose Desktop
-    implementation(compose.desktop.currentOs)
-    implementation(compose.material3)
-    implementation(compose.materialIconsExtended)
-
-    // Apache POI for Excel
-    implementation("org.apache.poi:poi:5.2.5")
-    implementation("org.apache.poi:poi-ooxml:5.2.5")
-
-    // iText for PDF
-    implementation("com.itextpdf:itext7-core:7.2.5")
-    implementation("com.itextpdf:kernel:7.2.5")
-    implementation("com.itextpdf:layout:7.2.5")
-
-    // Gson for XML/JSON
-    implementation("com.google.code.gson:gson:2.10.1")
-
-    // Kotlin Coroutines
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-swing:1.7.3")
-
-    // Logging
-    implementation("org.slf4j:slf4j-simple:2.0.9")
-}
-
-compose.desktop {
-    application {
-        mainClass = "gradecalculator.MainKt"
-        nativeDistributions {
-            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
-            packageName = "GradeCalculator"
-            packageVersion = "1.0.0"
-        }
-    }
-}
-
-kotlin {
-    jvmToolchain(17)
+    testImplementation(kotlin("test"))
 }
